7dc8f5d73a9d81d9092c93157e3a5c7ff708e8fa
dianlujitao <dianlujitao@lineageos.org>
2022-12-14 13:53:24 -0700

    msm8916-common: Drop Snap
    Snap is deprecated in LineageOS in favour of Aperature.

--- msm8916.mk
+++ msm8916.mk
@@ -79,8 +79,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     libcamera_shim \
-    camera.msm8916 \
-    Snap
+    camera.msm8916
 
 # Connectivity Engine support
 PRODUCT_PACKAGES += \
