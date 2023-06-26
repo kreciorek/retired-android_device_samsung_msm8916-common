dc72128d50729c2972d0419c5e93d706ae4cda3d
retiredtab-gerrit <you@example.com>
2023-06-26 14:07:48 -0600

    msm8916-common: update selinux directories and compiler syntax
    This was commented out in 19.1 because I couldn't make selinux work.
    
    It now works in 20, so let's enable them and update the compiler syntax.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -193,10 +193,10 @@ TARGET_RECOVERY_FSTAB := $(COMMON_PATH)/rootdir/etc/fstab.qcom
 TARGET_RECOVERY_PIXEL_FORMAT	:= "RGB_565"
 
 # SELinux
-#include device/qcom/sepolicy-legacy/sepolicy.mk
-#BOARD_VENDOR_SEPOLICY_DIRS +=  $(COMMON_PATH)/sepolicy/vendor
-#BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/private
-BOARD_VENDOR_SEPOLICY_DIRS +=  $(COMMON_PATH)/sepolicy_tmp
+include device/qcom/sepolicy-legacy/sepolicy.mk
+BOARD_VENDOR_SEPOLICY_DIRS +=  $(COMMON_PATH)/sepolicy/vendor
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/private
+#BOARD_VENDOR_SEPOLICY_DIRS +=  $(COMMON_PATH)/sepolicy_tmp
 
 # Shims
 TARGET_LD_SHIM_LIBS := \
